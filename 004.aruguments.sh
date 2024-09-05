68d762fbf13d8ce1942da04c099097cc2bbbd21c
chaitanya12206 <chaitanya12201@gmail.com>
2024-09-06 00:47:10 +0530

    variables

---
+++ 004.aruguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi How are you doing"
+echo "${PERSON2} :: Hi i am doing good how are you"
+echo "$PERSON1 :: i am doing well "
+echo "${PERSON2} :: i am learning devops"  
