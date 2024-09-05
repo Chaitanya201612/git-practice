76ef0b3f238cc8e724c898dc392fd3d40143c28a
chaitanya12206 <chaitanya12201@gmail.com>
2024-09-06 00:35:23 +0530

    variables

---
+++ 003.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 :: Hi How are you doing"
+echo "${PERSON2} :: Hi i am doing good how are you"
+echo "$PERSON1 :: i am doing well "
+echo "${PERSON2} :: i am learning devops"  
