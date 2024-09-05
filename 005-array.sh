5f9b3dce030a5020f9d5d872c20ec9a2054e92f3
chaitanya12206 <chaitanya12201@gmail.com>
2024-09-06 01:04:08 +0530

    variables

---
+++ 005-array.sh
@@ -0,0 +1,12 @@
+#!?bin/bash
+
+# index starts from 0 ,size is 4
+
+FRUITS=("APPLE" "KIWI" "ORANGE" "BERRY") #Array
+
+echo "First fruti is : ${Fruits[0]}"
+echo "Second fruti is : ${Fruits[1]}"
+echo "Third fruti is : ${Fruits[2]}"
+echo "Fourth fruti is : ${Fruits[3]}"
+
+echo "All fruti at  a time : ${Fruits[@]}"
